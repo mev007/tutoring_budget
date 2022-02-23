557bd938976e2aaff030e57686765e2d35575a67
Yevhen <mev007@ukr.net>
2022-02-23 17:49:50 +0200

    Revert "add fFinanceScreen"
    This reverts commit 994096be5a2015cfe5327f63763699a94c4ad63d.

--- lib/screen/board/board_screen.dart
+++ lib/screen/board/board_screen.dart
@@ -2,7 +2,6 @@ import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tutoring_budget/constants/constants.dart';
-import 'package:tutoring_budget/screen/finance/finance_screen.dart';
 import 'package:tutoring_budget/screen/lessons/lessons_screen.dart';
 import 'package:tutoring_budget/screen/student/student_screen.dart';
 import 'package:tutoring_budget/utils.dart';
@@ -28,7 +27,7 @@ class BoardScreen extends StatelessWidget {
               index: ctrl.tabIndex,
               children: [
                 LessonsScreen(),
-                FinanceScreen(),
+                LessonsScreen(),
                 StudentScreen(),
               ],
             ),
